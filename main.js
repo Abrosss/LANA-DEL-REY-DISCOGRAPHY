f1eaf943e74f2ff8b809170935e48adabcf6100c
Abrosss <nataroro5@gmail.com>
2022-05-28 23:45:34 +0600

    swiper

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+
+  direction: 'vertical',
+  speed:1000,
+  mousewheelControl: true,
+  mousewheel: true,
+  slidesPerView: 1,
+  // freeMode: true,
+  // freeModeSticky: true
+});
