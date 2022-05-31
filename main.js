f386978f99980bbd7687c276877b2df3187e1c4d
Abrosss <nataroro5@gmail.com>
2022-05-31 22:23:19 +0600

    new

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-// let names =['Blue Banisters', 'Chemtrails over the Country Club', 'Norman Fucking Rockwell', 'Lust for Life',  'Ultraviolence', 'Honeymoon',  'Born to die']
-var keys = ['Air', 'Earth', 'Water', 'Fire', 'Love'];
+let names =['Blue Banisters', 'Violet Bent Backwards Over the Grass', 'Chemtrails over the Country Club', 'Norman Fucking Rockwell', 'Lust for Life',  'Ultraviolence', 'Honeymoon',  'Born to die']
+
+if(window.innerWidth>1900){
 var swiper = new Swiper('.swiper-container', {
   
   direction: 'vertical',
-  speed:400,
+  speed:800,
   mousewheelControl: true,
   mousewheel: true,
   slidesPerView: 1,
   allowTouchMove: false,
+  hashNavigation: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '\
+        <div class="box ' + className + '">\
+        <div class="text">' + (names[index]) + '</div>\
+        </div>';
+    },
+  },
   
   // freeMode: true,
   // freeModeSticky: true
 });
+}
--- script.js
+++ script.js
@@ -1,39 +1,70 @@
+let audio = document.getElementById('my_audio')
+window.onload = function() {
+    audio.play();
+}
 
 const albumArray = [
     {
         name: 'Blue Banisters',
-        img: 'blue banisters.webp',
-        color: 'blue'
+        hash: 'bluebanisters',
+        img: '/album covers/blue banisters.webp',
+        audio: '/background audio/arcadia.mp3',
+        video: '/background videos/blueee.mp4',
+        gif: 'bluegif.mp4',
+        color: 'blue',
+    },
+    {
+        name: 'Violet Bent Backwards Over the Grass',
+        hash: 'violetbentbackwardsoverthegrass',
+        img: '/album covers/VBBOTG.webp'
     },
     {
         name: 'Chemtrails over the Country Club',
-        img: 'chemtrails.webp',
+        hash: 'chemtrails',
+        img: '/album covers/chemtrails.webp',
+        audio: '/background audio/whitedress.mp3',
+        video: '/background videos/chem.mp4',
         color: 'grey'
     },
     {
         name: 'Norman Fucking Rockwell',
-        img: 'NormanFuckingRockwell.webp',
+        hash: 'normanfuckingrockwell',
+        img: '/album covers/NormanFuckingRockwell.webp',
+        video: '/background videos/greatt.mp4',
+        audio: '/background audio/norman.mp3',
         color: 'green'
     },
     {
         name: 'Lust for Life',
-        img: 'lust for life.webp',
+        hash: 'lustforlife',
+        img: '/album covers/lust for life.webp',
+        audio: '/background audio/whitemustang.mp3',
+        video: '/background videos/lust.mp4',
         color: 'white'
     },
     {
         name: 'Ultraviolence',
-        img: 'Ultraviolence.webp',
-        color: 'black'
+        hash: 'ultraviolence',
+        img: '/album covers/Ultraviolence.webp',
+        color: 'black',
+        video: '/background videos/ultraviolence.mp4',
+        audio: '/background audio/moneypowerglory.mp3'
     },
     {
         name: 'Honeymoon',
-        img: 'Honeymoon.webp',
-        color: 'aqua'
+        hash: 'honeymoon',
+        img: '/album covers/Honeymoon.webp',
+        color: 'aqua',
+        video: '/background videos/honeymoon.mp4',
+        audio: '/background audio/honeymoon.mp3'
     },
     {
         name: 'Born to die',
-        img: 'BornToDie.webp',
-        color: 'brown'
+        hash: 'borntodie',
+        img: '/album covers/paradise.jpg',
+        color: 'brown',
+        video: '/background videos/ride.mp4',
+        audio: '/background audio/ride.mp3'
     },
    
 ]
@@ -49,12 +80,12 @@ function createBoard(){
         info.classList.add('inside')
         const album = document.createElement('img')
         album.setAttribute('src', albumArray[i].img)
-        album.setAttribute('data-index', i)
+        container.setAttribute('data-hash', albumArray[i].hash)
         const name = document.createElement('h1')
         const link = document.createElement('span')
-        const menuLink = document.createElement('li')
-        menuLink.innerHTML=albumArray[i].name
-        menu.appendChild(menuLink)
+        // const menuLink = document.createElement('li')
+        // menuLink.innerHTML=albumArray[i].name
+        // menu.appendChild(menuLink)
         link.innerHTML='BUY HERE'
         name.innerHTML=albumArray[i].name
         info.appendChild(album)
@@ -66,9 +97,38 @@ function createBoard(){
     }
 }
 createBoard()
+let vid = document.getElementById("myVideo");
 
 swiper.on('transitionStart', function (e) {
     albumArray.forEach((album, index) => {
-        if(index==swiper.activeIndex)
-        document.body.style.background=album.color})
+        if(index==swiper.activeIndex){
+        vid.src = album.video
+        audio.src = album.audio
+        console.log(album.audio)
+        audio.play()
+        }
+        // document.body.style.background=album.color 
+    })
 });
+
+
+window.addEventListener('hashchange', function() {
+    albumArray.forEach((album, index) =>{
+        const hash = this.location.hash.replace('#', '')
+        if(hash==album.hash)
+        return swiper.slideTo(index)
+        })})
+        
+    
+  
+
+        document.addEventListener('keyup', event => {
+            if (event.code === 'Space') {
+                swiper.slideNext(800); //whatever you want to do when space is pressed
+                // if(swiper.activeIndex==swiper.slides.length-1)
+                // console.log('last')
+            }
+            // if(event.code === 'Space' && swiper.activeIndex===swiper.slides.length-1){
+            //     swiper.slideTo(0)
+            // }
+          })
